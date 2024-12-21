24b806207e4b2d1fa8ef9288ae9816e67440dde4
Victoria6789 <152925832+Victoria6789@users.noreply.github.com>
2024-12-21 19:43:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://victoria6789.github.io/cv",
+  base: "https://victoria6789.github.io/cv_1",
 })
